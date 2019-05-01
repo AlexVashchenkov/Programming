8fda2cbe461aa909fae8c2703b3b737d8af22196
Alex <alex.vashchenkov@gmail.com>
2019-05-01 10:42:51 +0300

    Initial

--- 20190424_D/2.ml
+++ 20190424_D/2.ml
@@ -17,13 +17,15 @@ let rec create n k =
 
 let rec parse a depth length = 
 	match a with 
- L -> if depth = 0 then print_string "+--L\n" else (if depth = 1 then print_string ((make length ' ') ^ "+" ^ (make (length-1) '-') ^ "L\n") else  print_string ((create (depth-1) 0) ^ "   +--L\n"));
-|N (n,t1,t2) -> if depth = 0 then print_string ("+" ^ (make (length-2) '-') ^ "()\n") else (if depth = 1 then print_string ((make length ' ') ^ "+" ^ (make (length-2) '-') ^ "()\n") else  print_string ((create (depth-1) 0) ^ "   +--()\n"));
-		print_string ((create (depth+1) 0) ^ "\n");
-			(parse t1 (depth + 1) length);
-			(parse t2 (depth + 1) length);;
-
+ L -> if depth = 0 then print_string "+---L\n" else (if depth = 1 then print_string ((make length ' ') ^ "+" ^ (make (length-1) '-') ^ "L\n") else  print_string ((create (depth-1) 0) ^ "   +---L\n"));
+|N (n,t1,t2) -> if depth = 0 then print_string ("+" ^ (make (length-2) '-') ^ "()\n") else 
+	       (if depth = 1 then print_string ((make length ' ') ^ "+" ^ (make (length-2) '-') ^ "()\n") else  
+					print_string ((create (depth-1) 0) ^ "   +--()\n"));
+					print_string ((create (depth+1) 0) ^ "\n");
+					(parse t1 (depth+1) length);
+					print_string ((create (depth+1) 0) ^ "\n");
+					(parse t2 (depth+1) length);;
 let rec create_tree n = 
 	if n < 0 then L else (N (n,(create_tree (n-1)),L));;
 
-parse (create_tree 4) 0 5;;	
+parse (create_tree 4) 0 4;;	
