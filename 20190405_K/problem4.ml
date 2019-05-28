69227c364254a10f11174a0a1322e366d966e5b4
Alex <alex.vashchenkov@gmail.com>
2019-05-28 21:23:40 +0300

    Initial

--- 20190405_K/problem4.ml
+++ 20190405_K/problem4.ml
@@ -1,26 +1,19 @@
 open Array;;
 
-let n = 13;;
-
-let rec swap a y x n = 
-	a.(x).(y) <- n;a;;
+let n = read_int();;
 
 let a = init n (fun x -> init n (fun y -> " "));;
 
-let rec draw_vert a x y1 y2 = 
-	if y1 = y2 then a else (swap a x y1 "*";draw_vert a x (y1+1) y2);;
+let put a x y = a.(y).(x) <- "*";a;;
 
-let rec draw_gor a x1 x2 y = 
-	if x1 = x2 then a else (swap a x1 y "*";draw_gor a (x1+1) x2 y);;
+let rec draw_line s x y a b c = 
+	if y = n then s else
+	if x = n then (draw_line s 0 (y+1) a b c) else 
+	if a*x+b*y+c=0 then (draw_line (put s x y) (x+1) y a b c) else (draw_line s (x+1) y a b c);; 
+	
+draw_line a 0 0 1 0 (-(n/2));;
+draw_line a 0 0 0 1 (-(n/2));;
+draw_line a 0 0 0 1 0;;
+draw_line a 0 0 0 1 (-n+1);;
 
-let rec draw_line a x1 y1 x2 y2 x = 
-	let k = (y2 - y1 / x2 - x1) in
-	let b = y2 - k * x1 in
-	if x1 = x2 then (swap a x y2 "*";draw_line a x1 y1 x2 y2 (x+1)) else
-	(swap a (k * x + b) y1 "*";draw_line a x1 y1 x2 y2 (x+1));;
-
-let draw_king a = 
-	draw_gor (draw_gor (draw_gor (draw_vert a (n / 2) 0 (n-1)) 0 n (n / 2)) 0 n (n-1)) 0 n 0;;
-
-iter (fun x -> iter (fun y -> print_string y) x;print_string "\n") (draw_king a);;
-
+iter (fun x -> iter (fun y -> print_string (y ^ " ")) x;print_string "\n") a;;
--- 20190405_K/problem5.ml
+++ 20190405_K/problem5.ml
@@ -1,26 +1,33 @@
 open Array;;
 
-let n = 13;;
-
-let rec swap a y x n = 
-	a.(x).(y) <- n;a;;
+let n = 11;;
 
 let a = init n (fun x -> init n (fun y -> " "));;
 
-let rec draw_vert a x y1 y2 = 
-	if y1 = y2 then a else (swap a x y1 "*";draw_vert a x (y1+1) y2);;
+let put a x y = a.(y).(x) <- "*";a;;
 
-let rec draw_gor a x1 x2 y = 
-	if x1 = x2 then a else (swap a x1 y "*";draw_gor a (x1+1) x2 y);;
+let rec draw_line s x y a b c = 
+	if y = n then s else
+	if x = n then (draw_line s 0 (y+1) a b c) else 
+	if a*x+b*y+c=0 then (draw_line (put s x y) (x+1) y a b c) else (draw_line s (x+1) y a b c);; 
 
-let rec draw_line a x1 y1 x2 y2 x = 
-	let k = (y2 - y1 / x2 - x1) in
-	let b = y1 - k * x1 in
-	if x1 = x2 then (swap a x y2 "*";draw_line a x1 y1 x2 y2 (x+1)) else
-	(swap a (k * x + b) y1 "*";draw_line a x1 y1 x2 y2 (x+1));;
+let rec draw_int s x y a b c p q = 
+	if y = n then a else
+	if x = p then (draw_int s q (y+1) a b c p q) else
+	if a*x+b*y+c=0 then (draw_int (put s x y) (x+1) y a b c p q) else (draw_int s (x+1) y a b c p q);; 
 
-let draw_gold a = 
-	draw_line a 0 (n/2) (n/2) 0 0;;	
+let rec draw_list s x y l = 
+	match l with
+ [] -> s
+|a :: b -> if x = n then (draw_list s 0 (y+1) l) else (put s x y;draw_list s (x+1) y b);;
 
-iter (fun x -> iter (fun y -> print_string y) x;print_string "\n") (draw_line a 0 (n/2) (n/2) 0 0);;
+let rec create n = 
+	if n = 0 then [] else "*" :: (create (n-1));;
+ 
+draw_line a 0 0 1 (-1) (-(n-1)/2);;
+draw_line a 0 0 1 1 (-(n-1)/2);;
+draw_line a 0 0 0 1 (-(n-1));;
+draw_line a 0 0 1 0 (-n/2);;
+draw_list a ((n - (List.length (create (n-4)))) / 2) (n-3) (create (n-4));;	
 
+iter (fun x -> iter (fun y -> print_string (y ^ " ")) x;print_string "\n") a;;
